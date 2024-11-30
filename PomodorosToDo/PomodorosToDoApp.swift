c4c231323ae63ceaadc5ccb6b07f72d5fe663fb6
Tyler Morris <tyler.david.morris@gmail.com>
2024-11-29 21:07:28 -0600

    first commit

---
+++ PomodorosToDo/PomodorosToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodorosToDoApp.swift
+//  PomodorosToDo
+//
+//  Created by Tyler Morris on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct PomodorosToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Pomodoros To Do
+
+A relatively simple app for using the Pomodoro method to operationalize a to-do list. Allows users to set and track daily goal progress in terms of Pomodoros.
